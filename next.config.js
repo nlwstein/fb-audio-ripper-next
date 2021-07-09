8c27bd1822303f41598f8524e20af8d73d58169e
Nicholas Stein <nick@nickstein.net>
2021-07-09 04:48:03 -0400

    Testing again

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Important: return the modified config
+      
+      config.externals.push("bufferutil", "utf-8-validate"); 
+      return config
+    },
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 
 
   module.exports = {
-    externals: {
-    "bufferutil": "bufferutil",
-    "utf-8-validate": "utf-8-validate"
-    }
+
   }
