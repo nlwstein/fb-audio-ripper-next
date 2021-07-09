a472967916850f04f38d6d912e33a2a216d8769a
Nicholas Stein <nick@nickstein.net>
2021-07-09 04:55:53 -0400

    Reverted

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // Important: return the modified config
-      
-      config.externals.push("bufferutil", "utf-8-validate", "mqtt"); 
-      return config
-    },
-  }
