806b87f83d8fe162c8fa6a1c0a741b2644e69f97
Nicholas Stein <nick@nickstein.net>
2021-07-09 04:50:55 -0400

    Added another one

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       // Important: return the modified config
       
-      config.externals.push("bufferutil", "utf-8-validate"); 
+      config.externals.push("bufferutil", "utf-8-validate", "mqtt"); 
       return config
     },
   }
